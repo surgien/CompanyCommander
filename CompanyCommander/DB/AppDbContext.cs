510f0020b9137937eb4960180b92bac707b8bf4c
urgien <stephan.urgien@unitrade.com>
2025-01-07 21:34:40 +0100

    Counter speichert nun im Local Storage

---
+++ CompanyCommander/DB/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+//namespace CompanyCommander.DB {
+//  public class Stockpile {
+//    public int Round { get; set; }
+//    public int Amount { get; set; }
+//  }
+
+//  public class Income {
+//    public int Round { get; set; }
+//    public int Amount { get; set; }
+//  }
+
+//  public class AppDbContext : DbContext {
+//    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+//    public DbSet<Income> Income { get; set; }
+//    public DbSet<Stockpile> Stockpile { get; set; }
+//  }
+//}
