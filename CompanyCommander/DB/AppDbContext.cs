23f3ed19945488a6ec1760925c82b5ace22c09c2
urgien <stephan.urgien@unitrade.com>
2025-01-08 00:46:39 +0100

    Buildingboard ausprgrammiert: Counter für Ressourcen

--- CompanyCommander/DB/AppDbContext.cs
+++ CompanyCommander/DB/AppDbContext.cs
@@ -1,20 +1,86 @@
 ﻿//using Microsoft.EntityFrameworkCore;
 
-//namespace CompanyCommander.DB {
-//  public class Stockpile {
-//    public int Round { get; set; }
-//    public int Amount { get; set; }
-//  }
+using System.Runtime.CompilerServices;
+using Blazored.LocalStorage;
+using LiteDB;
 
-//  public class Income {
-//    public int Round { get; set; }
-//    public int Amount { get; set; }
-//  }
+namespace CompanyCommander.DB {
 
-//  public class AppDbContext : DbContext {
-//    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+  //HÄÄÄÄÄÄÄ????
+  //public enum StockpileType {
+  //  Food,
+  //  Ammo,
+  //  Fuel
+  //}
 
-//    public DbSet<Income> Income { get; set; }
-//    public DbSet<Stockpile> Stockpile { get; set; }
-//  }
-//}
+  public enum StockpileType {
+    Manpower,
+    Ammo,
+    Fuel,
+    VictoryPoints //er auch ...
+  }
+
+  public class Stockpile {
+    public int Round { get; set; }
+    public int Amount { get; set; }
+    public StockpileType Type { get; set; }
+    [BsonId]
+    public ObjectId Id { get; set; }
+  }
+
+  public class Income {
+    public int Round { get; set; }
+    public int Amount { get; set; }
+    public StockpileType Type { get; set; }
+    [BsonId]
+    public ObjectId Id { get; set; }
+  }
+
+  public class AppDbContext {
+
+    public ILiteCollection<Stockpile> Stockpile => Database.GetCollection<Stockpile>();
+    public ILiteCollection<Income> Income => Database.GetCollection<Income>();
+
+
+
+    public LiteDatabase Database { get; set; }
+
+    public AppDbContext(ILocalStorageService localStorage) {
+
+      _localStorage = localStorage;
+    }
+
+    private readonly ILocalStorageService _localStorage;
+    private MemoryStream _memoryStream;
+
+    public async Task LoadDatabaseAsync() {
+      var dbContent = await _localStorage.GetItemAsync<string>(nameof(AppDbContext));
+
+      _memoryStream = new MemoryStream();
+
+      if (dbContent != null) {
+        var bytes = Convert.FromBase64String(dbContent);
+        _memoryStream = new MemoryStream(bytes);
+      }
+
+      Database = new LiteDatabase(_memoryStream);
+    }
+
+    public async Task SaveDatabaseAsync() {
+      Database.Commit();
+      Database.Checkpoint();
+
+      _memoryStream.Position = 0;
+
+      var jsonString = Convert.ToBase64String(_memoryStream.ToArray());
+      await _localStorage.SetItemAsync(nameof(AppDbContext), jsonString);
+    }
+  }
+
+  //public class AppDbContext : DbContext {
+  //  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+  //  public DbSet<Income> Income { get; set; }
+  //  public DbSet<Stockpile> Stockpile { get; set; }
+  //}
+}
--- CompanyCommander/Program.cs
+++ CompanyCommander/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using CompanyCommander;
+using CompanyCommander.DB;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<AppDbContext>();
 
 await builder.Build().RunAsync();
