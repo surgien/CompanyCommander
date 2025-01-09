18f7aa148595c9705cc1eac486c36cf8a063ee50
Stephan Urgien <stephan.urgien@outlook.de>
2025-01-10 00:35:50 +0100

    Refactoring

---
+++ CompanyCommander/Model/RoundInfoModel.cs
@@ -0,0 +1,11 @@
+﻿using CompanyCommander.DB;
+
+namespace CompanyCommander.Model {
+  public class RoundInfoModel {
+    public DateTime Date { get; set; }
+    public int Round { get; set; }
+    public int FukCount { get; set; }
+    public List<Income>? Income { get; set; }
+    public List<Stockpile>? Stockpile { get; set; }
+  }
+}
