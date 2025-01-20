0f291602c928b76e6e2b7801227549df1fe64798
Stephan Urgien <stephan.urgien@outlook.de>
2025-01-21 00:36:36 +0100

    Prototyp für Verlauf wechseln

---
+++ CompanyCommander/Model/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace CompanyCommander.Model {
+  public class Customer {
+    public Customer(int id, string name) {
+      CustomerName = name;
+    }
+    public string CustomerName { get; set; }
+  }
+}
